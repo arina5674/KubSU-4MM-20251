30cd9d99784f56857806841acad1a327798b4700
Arina Usacheva <usacheva.arina03@mail.ru>
2025-03-22 10:04:29 +0300

    part one

--- src/main/java/ru/kubsu/telegrambot/TelegrambotApplication.java
+++ src/main/java/ru/kubsu/telegrambot/TelegrambotApplication.java
@@ -2,13 +2,24 @@ package ru.kubsu.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import ru.kubsu.telegrambot.ai.deepseek.DeepSeekClient;
+import ru.kubsu.telegrambot.bot.MyAmazingBot;
 
 @SpringBootApplication
 public class TelegrambotApplication {
 
-	public static void main(String[] args) {
-
+	private static final String TELEGRAM_BOT_TOKEN = "7983066682:AAGdJrGsDJjQu_gufxi5pOqTwUJCix8EjfI";
+	public static void main(String[] args) throws Exception {
+		// Spring
 		SpringApplication.run(TelegrambotApplication.class, args);
-	}
 
+		// Telegram
+		TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
+		botsApplication.registerBot(TELEGRAM_BOT_TOKEN,
+				new MyAmazingBot(
+						new OkHttpTelegramClient(TELEGRAM_BOT_TOKEN),
+						new DeepSeekClient()));
+	}
 }
